d674ec9346be3c27d9e73d6e9aaefe88f2eef969
Marko1310 <marko.cabo13@gmail.com>
2023-01-09 23:19:48 +0100

    Basic layout

--- src/components/List.js
+++ src/components/List.js
@@ -2,9 +2,22 @@ import React from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
 const List = () => {
   return (
-    <div className="grocery-list">
-      <div className="section-center"></div>
-    </div>
+    <ul className="grocery-list-container">
+      <li className="grocery-list-item">
+        <div>fssfkls</div>
+        <div className="submit-buttons">
+          <button className="submit-btn edit">Edit</button>
+          <button className="submit-btn delete">Delete</button>
+        </div>
+      </li>
+      <li className="grocery-list-item">
+        <div>fssfkls</div>
+        <div className="submit-buttons">
+          <button className="submit-btn edit">Edit</button>
+          <button className="submit-btn delete">Delete</button>
+        </div>
+      </li>
+    </ul>
   );
 };
 
--- src/components/Submit.js
+++ src/components/Submit.js
@@ -1,16 +1,21 @@
 const Submit = () => {
   return (
-    <div className="submit-container">
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          console.log(e.target[0].value, e.target[1].value);
-        }}
-      >
-        <input type="text" id="grocery" name="grocery"></input>
-        <input type="submit" value="Submit"></input>
-      </form>
-    </div>
+    <form
+      className="submit-container"
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log(e.target[0].value, e.target[1].value);
+      }}
+    >
+      <input
+        type="text"
+        id="grocery"
+        name="grocery"
+        placeholder="e.g. banana"
+        className="submit-input"
+      ></input>
+      <input type="submit" value="Submit" className="submit-btn"></input>
+    </form>
   );
 };
 
