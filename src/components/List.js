c022b2e3402997f0a4f6a3655aaaa78ab60234d1
Marko1310 <marko.cabo13@gmail.com>
2023-01-10 09:41:16 +0100

    Main design complete

--- src/components/List.js
+++ src/components/List.js
@@ -2,22 +2,25 @@ import React from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
 const List = () => {
   return (
-    <ul className="grocery-list-container">
-      <li className="grocery-list-item">
-        <div>fssfkls</div>
-        <div className="submit-buttons">
-          <button className="submit-btn edit">Edit</button>
-          <button className="submit-btn delete">Delete</button>
-        </div>
-      </li>
-      <li className="grocery-list-item">
-        <div>fssfkls</div>
-        <div className="submit-buttons">
-          <button className="submit-btn edit">Edit</button>
-          <button className="submit-btn delete">Delete</button>
-        </div>
-      </li>
-    </ul>
+    <div className="grocery-list-container">
+      <ul>
+        <li className="grocery-list-item">
+          <div>fssfkls</div>
+          <div className="submit-buttons">
+            <button className="submit-btn edit">Confirm</button>
+            <button className="submit-btn delete">Delete</button>
+          </div>
+        </li>
+        <li className="grocery-list-item">
+          <div>fssfkls</div>
+          <div className="submit-buttons">
+            <button className="submit-btn edit">Edit</button>
+            <button className="submit-btn delete">Delete</button>
+          </div>
+        </li>
+      </ul>
+      <div className="clear-btn">Clear list</div>
+    </div>
   );
 };
 
