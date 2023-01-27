23a11d46e5066b48af8652e86b5ead05000101e0
Marko1310 <marko.cabo13@gmail.com>
2023-01-27 13:07:30 +0100

    adding signout to main page

--- root/frontend/src/components/navbar/Navbar.js
+++ root/frontend/src/components/navbar/Navbar.js
@@ -5,8 +5,13 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <div className="navbar-container">
-      <img src={logo} className="navbar-logo" />
-      <p className="navbar-title">MY GROCERY LIST</p>
+      <div className="navbar-left">
+        <img src={logo} alt="logo" className="navbar-logo" />
+        <p className="navbar-title">MY GROCERY LIST</p>
+      </div>
+      <div className="navbar-right">
+        <a className="navbar-signout">Signout</a>
+      </div>
     </div>
   );
 };
