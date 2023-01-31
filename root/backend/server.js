f72a1466448de81a54eca1d8120e79fbf254de4d
Marko1310 <marko.cabo13@gmail.com>
2023-01-31 18:18:50 +0100

    installed cors

--- root/backend/server.js
+++ root/backend/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const PORT = 3001;
 
 app.use(express.json());
+app.use(cors);
 
 const database = {
   users: [
