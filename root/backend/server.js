28a19b647beeff6be705633e46b487f0890e17d9
Marko1310 <marko.cabo13@gmail.com>
2023-02-05 23:37:29 +0100

    requesting specific user from db

--- root/backend/server.js
+++ root/backend/server.js
@@ -75,16 +75,16 @@ app.post("/register", (req, res) => {
 // get specific user
 app.get("/profile/:id", (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  });
-  if (!found) {
-    res.status(404).json("no such user");
-  }
+  db.select("*")
+    .from("users")
+    .where("id", id)
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("user not found");
+      }
+    });
 });
 
 //add new grocerie
